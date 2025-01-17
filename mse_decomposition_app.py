e9b28d3cb525858a818db2f6435d4cc044a4454f
Török Péter <torokpe@gmail.com>
2025-01-17 20:35:15 +0100

    Update mse_decomposition_app.py

--- mse_decomposition_app.py
+++ mse_decomposition_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+df = pd.read_csv('https://storage.googleapis.com/kagglesdsdata/datasets/5656167/9334344/house_price_regression_dataset.csv?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20250117%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20250117T141127Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=7355844dafbd6ff14cc019d15a8d613dfbc2b16a7c76fd7e494dd86efa5c77829dc432f2c1eee38004992da184da2996132be68e403b40912a232ce869f2ee446ec9f34f41270eeb5139c533e04ff032f8a30fd4d6dae7126114041ccd1d13eddae403e3ed91ce39d7ade7e5d3bf0b87a62c808764e054482a8cf9300cf743d0649d19396c4ef72bc801695157745251fa9d6f0b6a965c4261108052594e4e029284ef2f66d432cf72ece8f0be21f58ada29c5964e3f39113b7c94ff4936f3ec49c7902a6621dcdc8c38ddea74b9277f3f4ea9abdb9a8bafd4322b0d507d4fef0efe97e1a0f6f97b3b5b4c8b6bc53743fe54feb20db533543571610b054c0590')
+
 # Function to calculate MSE, Bias², and Variance
 def calculate_bias_variance(df, actual_col, predicted_cols):
     results = []

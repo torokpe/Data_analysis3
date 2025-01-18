a9ddde8d411dc1442ce98a672b88e3084e4ad54a
Török Péter <torokpe@gmail.com>
2025-01-18 13:24:58 +0100

    Update mse_decomposition_app.py

--- mse_decomposition_app.py
+++ mse_decomposition_app.py
@@ -17,8 +17,18 @@ df = load_data()
 
 # Sidebar: Add a title and description
 st.markdown("<h1 style='text-align: center;'>Model Performance & MSE Decomposition</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; font-size:18px;'>The purpose of this dashboard is to provide a visualization of the decomposition of Mean Squared Error (MSE) for three different predictive models. By breaking down MSE into its key components—Bias, Variance, and Irreducible Error—the dashboard allows users to understand how each model performs and where improvements might be made. The app is powered by an online dataset on house prices, where the predictor variables consist of various characteristics of the houses, such as size, location, and neighbourhood rating etc.</p>", unsafe_allow_html=True)
-
+st.markdown(
+    """
+    <p style='text-align: center; font-size:18px;'>
+        The purpose of this dashboard is to provide a visualization of the decomposition of Mean Squared Error (MSE) for three different predictive models. 
+        By breaking down MSE into its key components—Bias, Variance, and Irreducible Error—the dashboard allows users to understand how each model performs and where improvements might be made. 
+        The app is powered by an online dataset on house prices, where the predictor variables consist of various characteristics of the houses, such as size, location, and neighbourhood rating etc.
+        For more details, visit 
+        <a href='https://www.kaggle.com/datasets/prokshitha/home-value-insights' target='_blank' style='color:blue; text-decoration: none;'>this link</a>.
+    </p>
+    """,
+    unsafe_allow_html=True
+)
 st.sidebar.write("Use the control panel to set model specifications.")
 
 # Sidebar: Interactive Model Selection

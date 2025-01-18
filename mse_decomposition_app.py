1adc6df27781f40886a1f2bcd5264242111bdf6f
Török Péter <torokpe@gmail.com>
2025-01-18 13:19:55 +0100

    Update mse_decomposition_app.py

--- mse_decomposition_app.py
+++ mse_decomposition_app.py
@@ -125,7 +125,7 @@ if evaluation_method == "Direct Testing (Train-Test Split)":
 st.markdown("<h3 style='text-align: center;'>Model Performance Metrics</h3>", unsafe_allow_html=True)
 st.subheader(f"Actual vs Predicted")
 plt.figure(figsize=(8, 6))
-plt.scatter(test_data["House_Price"], test_data["Predicted"], alpha=0.7, label="Predicted", color="#156082")
+plt.scatter(test_data["House_Price"], test_data["Predicted"], alpha=0.5, label="Predicted", color="#156082")
 plt.plot([test_data["House_Price"].min(), test_data["House_Price"].max()],
          [test_data["House_Price"].min(), test_data["House_Price"].max()],
          color="#FFC000", linestyle="--", label="Perfect Prediction")
